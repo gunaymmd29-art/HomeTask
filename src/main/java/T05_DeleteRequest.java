116cf7b2a7418d1669a6d71459e1597b19ad675a
gunaymmd29-art <gunaymmd29@gmail.com>
2026-02-06 23:38:28 +0400

    test

--- src/main/java/T05_DeleteRequest.java
+++ src/main/java/T05_DeleteRequest.java
@@ -1,4 +1,8 @@
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class T05_DeleteRequest {
 //URL:
@@ -6,5 +10,19 @@ public class T05_DeleteRequest {
 //Test edin:- Status code 200 olmalidir- Response body bosh {} olmalidir
 //Ipucu: .delete(url) ve .body(equalTo("{}")) istifade edin
     @Test
-    pub
+    public void deleteRequest(){
+
+        //1Step request body ve endpoint yarat
+        String url = "https://jsonplaceholder.typicode.com/posts/8";
+        //2 Step expected data hazirla
+        //3Step Request gonder donen response yadda saxla
+        Response response =given().when().delete(url);
+        //5Step Assertions
+        response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body(equalTo("{}"));
+    }
+
 }
